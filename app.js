d21e53cec6bcf76a3aeca5cef59b14411d391b94
Филимонов Никита Евгеньевич <filimonov-ne@college.tepk.dev>
2021-02-27 14:20:39 +0700

    remove project

---
+++ app.js
@@ -0,0 +1,13 @@
+window.addEventListener('load' , function () {
+
+
+let countUser = document.querySelector('.count-user'),
+  countComp = document.querySelector('.count-comp'),
+  userField = document.querySelector('.user-field'),
+  compField = document.querySelector('.comp-field'),
+  sound = document.querySelector('.sound'),
+  play = document.querySelector('.play'),
+  fields = document.querySelectorAll('.field'),   
+
+
+});
